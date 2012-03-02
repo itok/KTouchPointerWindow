45479576a5c9f96230490df9e507e46b291630c0
itok <i@itok.jp>
2012-03-03 04:26:23 +0900

    add comments

--- KTouchPointerWindow.h
+++ KTouchPointerWindow.h
@@ -4,5 +4,13 @@
 //  Created by Ito Kei on 12/03/02.
 //  Copyright (c) 2012年 itok. All rights reserved.
 //
-
+/*
+ * call this function to start show pointer
+ * 
+ * ex)
+ * - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+ *      KTouchPointerWindowInstall();
+ *      ...
+ * }
+ */
 void KTouchPointerWindowInstall();
